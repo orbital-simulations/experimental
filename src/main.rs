44b7043a0a2b67db6f36b62fe8d8de999f9af40f
Jan Sipr <sipr.jan@gmail.com>
2023-12-28 20:41:49 +0100

    Super small scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use comfy::{simple_game, EngineContext, RED, GameLoop, egui, draw_circle, vec2, draw_text, WHITE, TextAlign};
+
+simple_game!("egui example", update);
+
+fn update(_c: &mut EngineContext) {
+    egui::Window::new("Simple egui window")
+        .anchor(egui::Align2::CENTER_CENTER, egui::vec2(0.0, 0.0))
+        .show(egui(), |ui| {
+            if ui.button("hello").hovered() {
+                ui.colored_label(RED.egui(), "from egui");
+            } else {
+                ui.label("from egui");
+            }
+        });
+
+    draw_circle(vec2(0.0, 5.0), 0.5, RED * 5.0, 0);
+
+    draw_text(
+        "Nice red glowing circle with the help of HDR bloom",
+        vec2(0.0, -2.0),
+        WHITE,
+        TextAlign::Center,
+    );
 }
